faefa9cc80bc572c2cea0e76f6be5a6c3b5cc5df
vladMezhevyi <vlad.mezhevyi@gmail.com>
2026-09-17 19:49:59 +0100

    chore: format existing code using prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'mv-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class App {
   protected readonly title = signal('mv-news');
