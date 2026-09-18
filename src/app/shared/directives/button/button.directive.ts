fbfc08c90ca5611864cd383930e486ebd8aaf776
vladMezhevyi <vlad.mezhevyi@gmail.com>
2026-09-18 17:06:17 +0100

    feat: allow applying button directive to anchors, handle a11y

--- src/app/shared/directives/button/button.directive.ts
+++ src/app/shared/directives/button/button.directive.ts
@@ -1,9 +1,46 @@
-import { Directive } from '@angular/core';
+import {
+  booleanAttribute,
+  computed,
+  Directive,
+  effect,
+  ElementRef,
+  inject,
+  input,
+} from '@angular/core';
 
 @Directive({
-  selector: 'button[mvButton]',
+  selector: 'button[mvButton], a[mvButton]',
   host: {
     class: 'mv-button',
+    '[attr.disabled]': 'buttonDisabled()',
+    '[attr.aria-disabled]': 'anchorDisabled()',
+    '[attr.tabindex]': 'anchorTabIndex()',
+    '(click)': 'onClick($event)',
   },
 })
-export class ButtonDirective {}
+export class ButtonDirective {
+  private readonly el = inject<ElementRef<HTMLButtonElement | HTMLAnchorElement>>(ElementRef);
+
+  readonly disabled = input<boolean>(false);
+
+  private readonly isAnchor = this.el.nativeElement.tagName === 'A';
+
+  protected readonly anchorDisabled = computed<string | null>(() =>
+    this.isAnchor && this.disabled() ? 'true' : null,
+  );
+
+  protected readonly buttonDisabled = computed<boolean | null>(() =>
+    !this.isAnchor && this.disabled() ? true : null,
+  );
+
+  protected readonly anchorTabIndex = computed<string | null>(() =>
+    this.anchorDisabled() ? '-1' : null,
+  );
+
+  protected onClick(e: Event): void {
+    if (!this.isAnchor || !this.disabled()) return;
+
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  }
+}
