bab4cd80322ee6413e7e49a48bb0a5b35bb99bf6
vladMezhevyi <vlad.mezhevyi@gmail.com>
2026-09-20 23:00:37 +0100

    feat: create theme toggle component

---
+++ src/app/shared/components/theme-toggler/theme-toggler.component.ts
@@ -0,0 +1,29 @@
+import { Component, computed, inject } from '@angular/core';
+import { ThemeService } from '../../services/theme/theme.service';
+import { ButtonDirective } from '../../directives/button/button.directive';
+import { IconComponent } from '../icon/icon.component';
+
+@Component({
+  selector: 'mv-theme-toggler',
+  imports: [ButtonDirective, IconComponent],
+  template: `
+    <button mvButton iconOnly [attr.aria-label]="label()" (click)="onToggle()">
+      <mv-icon [name]="icon()" />
+    </button>
+  `,
+})
+export class ThemeTogglerComponent {
+  private readonly themeService = inject(ThemeService);
+
+  protected readonly isDark = computed<boolean>(() => this.themeService.currentTheme() === 'dark');
+
+  protected readonly icon = computed<string>(() => (this.isDark() ? 'sun' : 'moon'));
+
+  protected readonly label = computed<string>(
+    () => `Switch to ${this.isDark() ? 'light' : 'dark'} theme`,
+  );
+
+  protected onToggle(): void {
+    this.themeService.toggleTheme();
+  }
+}
