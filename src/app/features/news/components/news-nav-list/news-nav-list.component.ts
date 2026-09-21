2e245783bda4df7dfbfa42570263298c0a85ce17
Vladyslav Mezhevyi <vlad.mezhevyi@gmail.com>
2026-09-21 12:06:02 +0100

    feat: create reusable news-nav-list component

---
+++ src/app/features/news/components/news-nav-list/news-nav-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, input } from '@angular/core';
+import { NewsNavItem } from '../../services/news-navigation/news-navigation.service';
+import { NewsNavItemComponent } from '../news-nav-item/news-nav-item.component';
+
+@Component({
+  selector: 'mv-news-nav-list',
+  imports: [NewsNavItemComponent],
+  templateUrl: './news-nav-list.component.html',
+  styleUrl: './news-nav-list.component.scss',
+  host: {
+    '[class]': 'orientation()'
+  }
+})
+export class NewsNavListComponent {
+  readonly navItems = input.required<NewsNavItem[]>();
+  readonly orientation = input<'horizontal' | 'vertical'>('horizontal');
+}
