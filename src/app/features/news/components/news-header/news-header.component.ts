6df7d75f1a83fa1109b3693dda2cc64dd481853d
Vladyslav Mezhevyi <vlad.mezhevyi@gmail.com>
2026-09-21 12:07:49 +0100

    refactor: use news-nav-list in header and sidenav components, apply aria-expanded attribute on sidenav toggler when it's opened

--- src/app/features/news/components/news-header/news-header.component.ts
+++ src/app/features/news/components/news-header/news-header.component.ts
@@ -3,20 +3,20 @@ import { RouterLink } from '@angular/router';
 import { ButtonDirective } from '../../../../shared/directives/button/button.directive';
 import { IconComponent } from '../../../../shared/components/icon/icon.component';
 import { NewsNavigationService } from '../../services/news-navigation/news-navigation.service';
-import { NewsNavItemComponent } from '../news-nav-item/news-nav-item.component';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs';
 import { ThemeTogglerComponent } from '../../../../shared/components/theme-toggler/theme-toggler.component';
+import { NewsNavListComponent } from '../news-nav-list/news-nav-list.component';
 
 @Component({
   selector: 'mv-news-header',
   imports: [
-    RouterLink,
     ButtonDirective,
     IconComponent,
-    NewsNavItemComponent,
     ThemeTogglerComponent,
+    NewsNavListComponent,
+    RouterLink,
   ],
   templateUrl: './news-header.component.html',
   styleUrl: './news-header.component.scss',
@@ -29,6 +29,7 @@ export class NewsHeaderComponent {
   private readonly breakpointObserver = inject(BreakpointObserver);
 
   protected readonly navItems = this.navigation.navItems;
+  protected readonly sidenavOpened = this.navigation.sidenavOpened;
 
   protected readonly isMobile = toSignal<boolean>(
     this.breakpointObserver.observe(`(max-width: 768px)`).pipe(map((state) => state.matches)),
